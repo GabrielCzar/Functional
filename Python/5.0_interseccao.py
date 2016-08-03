cdd4c43fbc9ca8ae89a58d37d568acbbe30217a0
Gabriel Cesar <gcal1@live.com>
2016-08-03 19:34:41 -0300

    Atualização dos Códigos 2.0

--- Python/5.0_interseccao.py
+++ Python/5.0_interseccao.py
@@ -5,16 +5,3 @@ def inter(L, L2):
 	return list(set(L).intersection(L2))
 
 print inter(map (int, raw_input().split()), map (int, raw_input().split()))
-
-"""
-#OU
-
-def inter(L, L2):
-	if L == []:
-		return
-	if len(filter(lambda x: x == L[0], L2)) != 0:
-		print L[0],
-	inter(L[1:], L2)
-
-inter (list (set (map (int, raw_input().split()))), list (set (map (int, raw_input().split()))))
-"""
