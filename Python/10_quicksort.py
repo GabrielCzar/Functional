4c53b920b97948076ae2f9c4b3db958e13a27bb5
Gabriel César <gcal1@live.com>
2016-11-01 02:54:15 +0200

    add other resolution

--- Python/10_quicksort.py
+++ Python/10_quicksort.py
@@ -7,4 +7,7 @@ def quicksort(lista):
 		return lista
 	return 	quicksort(filter(lambda x, y = lista[0]: x < y, lista)) + filter(lambda x, y = lista[0]: x == y, lista) + quicksort(filter(lambda x, y = lista[0]: x > y, lista)) 
 
+#Outra forma mas nao muito legivel
+def quicksort2(lista): return lista if len(lista) < 2 else (quicksort2(filter(lambda x, y=lista[0]: x < y, lista)) + filter(lambda x, y=lista[0]: x == y, lista) + quicksort2(filter(lambda x, y=lista[0]: x > y, lista)))
+
 print ' '.join(map(str, quicksort(map(int, raw_input().split()))))
